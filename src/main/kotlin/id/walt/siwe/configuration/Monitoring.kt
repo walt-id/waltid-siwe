2b5d041d13722cb2f65aa270a5e95602614e143e
KBurgmann <kb@gatgetron.net>
2022-05-25 21:30:14 +0200

    Update Monitoring.kt

--- src/main/kotlin/id/walt/siwe/configuration/Monitoring.kt
+++ src/main/kotlin/id/walt/siwe/configuration/Monitoring.kt
@@ -1,23 +1,44 @@
 package id.walt.siwe.configuration
 
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.http.*
-import io.ktor.server.plugins.callid.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
         level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
-        callIdMdc("call-id")
+        /*format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val path = call.request.path()
+            val headers = call.request.headers.entries().joinToString { "${it.key} -> [${it.value.joinToString()}]" }
+            val cookies = call.request.cookies.rawCookies.entries.joinToString { "${it.key} -> ${it.value}" }
+            val body = runBlocking { call.receiveOrNull<String>() }
+            val query = call.request.queryString()
+            val queryParamMap = call.request.queryParameters.entries().joinToString { "${it.key} -> [${it.value.joinToString()}]" }
+
+            val resHeaders = call.response.headers.allValues().entries().joinToString { "${it.key} -> [${it.value.joinToString()}]" }
+            """
+            |REQUEST DEBUG LOG:
+            |Request: $httpMethod [${path}]
+            |    Headers: $headers
+            |    Cookies: $cookies
+            |    Body as String or null: $body
+            |    QueryString: $query
+            |    QueryParams: $queryParamMap
+            |Response: [${status}]
+            |    Headers: $resHeaders
+            |----------------------------------------------------------------------------------""".trimMargin()
+        }
+        */
+        // filter { call -> call.request.path().startsWith("/") }
+        //callIdMdc("call-id")
     }
-    install(CallId) {
+    /*install(CallId) {
         header(HttpHeaders.XRequestId)
         verify { callId: String ->
             callId.isNotEmpty()
         }
-    }
+    }*/
 
 }
