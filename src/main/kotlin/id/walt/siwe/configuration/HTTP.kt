d7315f0483ddf61783f1c257bd46c470b51cf542
KBurgmann <kb@gatgetron.net>
2022-05-25 21:30:02 +0200

    Update CORS configuration and add auto head response

--- src/main/kotlin/id/walt/siwe/configuration/HTTP.kt
+++ src/main/kotlin/id/walt/siwe/configuration/HTTP.kt
@@ -1,17 +1,21 @@
 package id.walt.siwe.configuration
 
 import io.ktor.http.*
-import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
+    install(AutoHeadResponse)
     install(CORS) {
+        allowHost("localhost:8080")
+        allowCredentials = true
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.ContentType)
+        //anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
 
 }
