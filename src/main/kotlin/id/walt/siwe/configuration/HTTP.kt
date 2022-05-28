39a05827c14c7e7bbac41a8d1adf256e0661c52c
Philipp Potisk <phil@walt.id>
2022-05-28 15:43:41 +0200

    Loosened CORS settings

--- src/main/kotlin/id/walt/siwe/configuration/HTTP.kt
+++ src/main/kotlin/id/walt/siwe/configuration/HTTP.kt
@@ -8,14 +8,14 @@ import io.ktor.server.plugins.cors.routing.*
 fun Application.configureHTTP() {
     install(AutoHeadResponse)
     install(CORS) {
-        allowHost("localhost:3000")
         allowCredentials = true
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Patch)
         allowMethod(HttpMethod.Delete)
         allowHeader(HttpHeaders.ContentType)
-        //anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-    }
 
+        // allowHost("localhost:3000")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
 }
